4eff2d9a082e0767bb4f2a5773a6c9a92bdb093b
Alex <cainalex33@gmail.com>
2021-09-05 12:37:19 +0000

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 import {Hero} from "../pages/PageBlocks/Hero/Hero"
 import {About} from "../pages/PageBlocks/About/About" 
 import {Skills} from "../pages/PageBlocks/Skills/Skills"
