e193804551cee08c1933a3487412e86b91504fe8
Alex <cainalex33@gmail.com>
2021-09-05 14:42:09 +0000

    images fix

--- pages/PageBlocks/Hero/HeroImage.tsx
+++ pages/PageBlocks/Hero/HeroImage.tsx
@@ -5,7 +5,7 @@ export const HeroImage = () => {
     <div className="flex flex-wrap justify-center">
       <div className="px-4 w-10/20 sm:w-4/12">
         <Image
-          src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-4-470x470.png"
+          src="https://images.unsplash.com/photo-1553484771-898ed465e931?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3900&q=80"
           alt="..."
           className="h-auto max-w-full align-middle border-none rounded-full shadow-lg"
           width={1000}
