4d341168534ca86ffe99c41eb1216264abab4f05
Alex <cainalex33@gmail.com>
2021-09-05 14:16:23 +0000

    image fix

--- pages/PageBlocks/Hero/HeroImage.tsx
+++ pages/PageBlocks/Hero/HeroImage.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image'
+
 export const HeroImage = () => {
   return (
     <div className="flex flex-wrap justify-center">
       <div className="px-4 w-10/20 sm:w-4/12">
-        <img
+        <Image
           src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-4-470x470.png"
           alt="..."
           className="h-auto max-w-full align-middle border-none rounded-full shadow-lg"
